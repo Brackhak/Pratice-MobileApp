33ab5e29375c7c10bbf21736eda1e58b15714aee
Brackhak <chatdanai2543@hotmail.com>
2022-05-07 18:39:19 +0700

    Update main.dart
    Scafflod Help UI

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,12 @@ import "package:flutter/material.dart";
 void main() {
   var app = MaterialApp(
     title: "My App",
-    home: Text("Hello Dart"),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Demo Project"), //ใส่ข้อความให้ส่วนหัวของแอพ
+      ),
+      body: Text("Pratice Project Kod Hear"), //ใส่ช้อความให้กับส่วนเนื้อหา
+    ), //Scaffold ช่วยจัดหน้า
   );
   runApp(app);
 }
